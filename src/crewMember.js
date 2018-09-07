aa5751912f819637a9a34cf3bdc4f79e220c95df
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-09-07 08:44:12 +0100

    Jo

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,49 @@
-class CrewMember {}
+const crewMembers = []
+let crewMemberId = 0
+
+class CrewMember {
+
+  constructor(position, currentShip) {
+    this.id = ++crewMemberId
+    this.position = position
+    this.currentShip = currentShip ? currentShip : 'Looking for a Rig'
+  }
+
+  engageWarpDrive() {
+    if (this.position === 'Pilot' && this.currentShip === 'Looking for a Rig') {
+        return "had no effect"
+    } else if (this.position !== 'Pilot' ) {
+        return "had no effect"
+    } else if (this.position === 'Pilot' && this.currentShip.warpDrive === 'engaged') {
+        return this.currentShip.warpDrive = 'disengaged'
+    } else if (this.position === 'Pilot' && this.currentShip.warpDrive === 'disengaged') {
+        return this.currentShip.warpDrive = 'engaged'
+    }
+  }
+
+
+  setsInvisibility() {
+    if (this.position === 'Defender' && this.currentShip === 'Looking for a Rig') {
+        return "had no effect"
+    } else if (this.position !== 'Defender' ) {
+        return "had no effect"
+    } else if (this.position === 'Defender' && this.currentShip.cloaked === true) {
+        return this.currentShip.cloaked = false
+    } else if (this.position === 'Defender' && this.currentShip.cloaked === false) {
+        return this.currentShip.cloaked = true
+    }
+  }
+
+  chargePhasers() {
+    if (this.position === 'Gunner' && this.currentShip === 'Looking for a Rig') {
+        return "had no effect"
+    } else if (this.position !== 'Gunner' ) {
+        return "had no effect"
+    } else if (this.position === 'Gunner' && this.currentShip.pashersCharged === 'charged') {
+        return this.currentShip.pashersCharged = 'uncharged'
+    } else if (this.position === 'Gunner' && this.currentShip.pashersCharged === 'uncharged') {
+        return this.currentShip.pashersCharged = 'charged'
+    }
+  }
+
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,22 @@
-class Spaceship {}
+const spaceships = []
+let spaceshipId = 0
+
+class Spaceship {
+
+  constructor(name, crewMembers, phasers, shields) {
+    this.id = ++spaceshipId
+    this.name = name
+    this.phasers = phasers
+    this.phasersCharge = 'uncharged'
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = 'disengaged'
+    this.crew = crewMembers
+    crewMembers.forEach((crewmember) => {
+      crewmember.currentShip = this
+    })
+    this.docked = this.crew.length === 0 ? true : false
+  }
+
+
+  }
